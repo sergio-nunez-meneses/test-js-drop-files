2162df8b64ab6a97d089c6baf0722e06394daddb
sergio núñez meneses <63654322+sergio-nunez-meneses@users.noreply.github.com>
2021-03-08 23:16:02 +0100

    added draggedOver variable to prevent triggering multiple dragover events, and dropHandler function in script.js; updated input-container and submit-button styles

--- script.js
+++ script.js
@@ -1,26 +1,44 @@
-var click = getBy('id', 'clickToUpload');
-var drop = getBy('id', 'dropToUpload');
+var click       = getBy('id', 'clickToUpload'),
+    drop        = getBy('id', 'dropToUpload'),
+    dropText = getBy('class', 'drop-text')[0],
+    draggedOver = false;
 
 function getBy(attribute, value) {
-    if (attribute === 'tag') {
-        return document.getElementsByTagName(value);
-    } else if (attribute === 'id') {
-        return document.getElementById(value);
-    } else if (attribute === 'name') {
-        return document.getElementsByName(value)[0];
-    } else if (attribute === 'class') {
-        return document.getElementsByClassName(value);
-    } else if (attribute === 'query') {
-        return document.querySelector(value);
-    }
+	if (attribute === 'tag') {
+		return document.getElementsByTagName(value);
+	}
+	else if (attribute === 'id') {
+		return document.getElementById(value);
+	}
+	else if (attribute === 'name') {
+		return document.getElementsByName(value)[0];
+	}
+	else if (attribute === 'class') {
+		return document.getElementsByClassName(value);
+	}
+	else if (attribute === 'query') {
+		return document.querySelector(value);
+	}
+}
+
+function dropHandler(e) {
+	console.log(e.dataTransfer);
+
+	dropText.innerHTML = e.dataTransfer.files[0].name;
 }
 
 drop.addEventListener('dragover', (e) => {
-    e.preventDefault();
-    console.log('dragging file over...');
+	e.preventDefault();
+
+	if (!draggedOver) {
+		draggedOver = true;
+		console.log('dragging file over...');
+	}
 });
+
 drop.addEventListener('drop', (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    alert('file dropped...');
+	e.preventDefault();
+
+	dropHandler(e);
+	draggedOver = false;
 });
